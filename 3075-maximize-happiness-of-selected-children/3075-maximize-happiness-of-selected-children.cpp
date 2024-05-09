9f207c0f30fc7ff8b503e788e97bc26b1c8c8a43
toothless486 <46721886+toothless486@users.noreply.github.com>
2024-05-09 10:11:41 +0900

    Time: 148 ms (90.73%), Space: 107.2 MB (60.15%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,14 +1,13 @@
+#include <algorithm>
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         std::sort(happiness.begin(), happiness.end(), std::greater<int>());
         
-        long long minus = 0;
+        int minus = 0;
         long long sum = 0;
         for(long long i=0; i<k; i++) {
-            auto value = happiness[i] - minus;
-            if(value <= 0) value = 0;
-            sum += value;
+            sum += max(happiness[i] - minus, 0);
             minus++;
         }
         return sum;
